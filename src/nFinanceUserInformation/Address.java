51d9275240ae8a78954652d34fde3c05cf41a5bd
robertito121 <37167135+robertito121@users.noreply.github.com>
2019-09-19 21:31:56 -0400

    Added User, Customer and Address classes, generated Javadoc

---
+++ src/nFinanceUserInformation/Address.java
@@ -0,0 +1,106 @@
+package nFinanceUserInformation;
+
+public class Address {
+
+    private int streetNumber;
+    private String streetName;
+    private String city;
+    private String state;
+    private int zipCode;
+
+    /**
+     * Constructor
+     * @param streetNumber
+     * @param streetName
+     * @param city
+     * @param state
+     * @param zipCode
+     */
+    public Address(int streetNumber, String streetName, String city, String state, int zipCode) {
+        this.streetNumber = streetNumber;
+        this.streetName = streetName;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+    }
+
+    /**
+     * Gets the street number attribute of the Address Object
+     * @return the street number of the Address
+     */
+    public int getStreetNumber() {
+        return streetNumber;
+    }
+
+    /**
+     * Sets the street number attribute of the Address Object
+     * @param streetNumber
+     */
+    public void setStreetNumber(int streetNumber) {
+        this.streetNumber = streetNumber;
+    }
+
+    /**
+     * Gets the street name attribute of the Address Object
+     * @return the street name of the address
+     */
+    public String getStreetName() {
+        return streetName;
+    }
+
+    /**
+     * Sets the street name attribute of the Address Object
+     * @param streetName
+     */
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+
+    /**
+     * Gets the city attribute of the Address Object
+     * @return city of the address
+     */
+    public String getCity() {
+        return city;
+    }
+
+    /**
+     * Sets the city attribute of the Address Object
+     * @param city
+     */
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    /**
+     * Gets the state attribute of the Address Object
+     * @return the state of the address
+     */
+    public String getState() {
+        return state;
+    }
+
+    /**
+     * Sets the state attribute of the Address Object
+     * @param state
+     */
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    /**
+     * Gets the zip code attribute of the Address Object
+     * @return the zip code of the address
+     */
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    /**
+     * Sets the zip code attribute of the Address Object
+     * @param zipCode
+     */
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
+    }
+}
--- src/nFinanceUserInformation/User.java
+++ src/nFinanceUserInformation/User.java
@@ -1,4 +1,49 @@
 package nFinanceUserInformation;
 
 public class User {
+
+    private String firstName;
+    private String lastName;
+
+    /**
+     * Constructor
+     * @param firstName
+     * @param lastName
+     */
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    /**
+     * Get the FirstName attribute of the user
+     * @return FirstName of the user
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * Sets the FirstName attribute of the user
+     * @param firstName
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * Gets the lastName attribute of the user
+     * @return LastName of the user
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * Sets the LastName attribute of the user
+     * @param lastName
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
