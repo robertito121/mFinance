15c90aff3f2241e10a21761f1c1f9ea1c66d8248
ParkerC8 <32624016+ParkerC8@users.noreply.github.com>
2019-09-22 20:40:29 -0400

    Update Loan.java

--- src/nFinanceProductInformation/Loan.java
+++ src/nFinanceProductInformation/Loan.java
@@ -1,4 +1,56 @@
 package nFinanceProductInformation;
 
 public class Loan {
+    
+    //Loan: loanType, loanNumber
+    private String loanType;
+    private int loanNumber;
+    
+    
+    /**
+     * Constructor
+     * @param loanType;
+     * @param loanNumber;
+     */
+    public Loan(String loanType, int loanNumber) {
+        this.loanType= loanType;
+        this.loanNumber = loanNumber;
+    }
+    
+    /**
+     * Gets the loanType attribute of the Loan
+     * @return the Loans loanType
+     * @see loanType
+     */
+    public String getLoanType() {
+        return loanType;
+    }
+    
+    /**
+     * Gets the loanNumber attribute of the Loan
+     * @return the Loans loanNumber
+     * @see loanNumber
+     */
+    public int getLoanNumber() {
+        return loanNumber;
+    }
+
+    /**
+     * Sets the loanType attribute of the loan
+     * @param loanType 
+     */
+    public void setLoanType(String loanType) {
+        this.loanType = loanType;
+    }
+
+    /**
+     * Sets the loanNumber attribute of the loan
+     * @param loanNumber 
+     */
+    public void setLoanNumber(int loanNumber) {
+        this.loanNumber = loanNumber;
+    }
+    
+    
+    
 }
